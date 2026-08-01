76d16d4a172be8db40e5a2ce0b56fe59cda733a7
Kashifmalik2007 <kashif71malik@gmail.com>
2026-08-01 18:02:21 +0500

    Update server.js

--- multiline-promotional-v2/server.js
+++ multiline-promotional-v2/server.js
@@ -68,10 +68,10 @@ const pool = connectionString
     })
   : mysql.createPool({
       host: process.env.DB_HOST || "sakura.proxy.rlwy.net",
-      port: Number(process.env.DB_PORT || 59768),
+      port: Number(process.env.DB_PORT || 3306),
       user: process.env.DB_USER || "root",
-      password: process.env.DB_PASSWORD || "",
-      database: process.env.DB_NAME || "railway",
+      password: process.env.DB_PASSWORD || "Kashifmalik@2007",
+      database: process.env.DB_NAME || "multiline_promotional",
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0,
